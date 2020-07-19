df117037e14bea6164832cdd51e52a47019729fc
kristall-93 <kristushapaf@yandex.ru>
2020-07-19 22:06:44 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require('gulp'),
     cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
-    return gulp.src('app/scss/style.scss')
+    return gulp.src('app/scss/**/*.scss')
       .pipe(sass({outputStyle: 'compressed'}))
       .pipe(rename({suffix: '.min'}))
       .pipe(autoprefixer({
@@ -21,7 +21,7 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
